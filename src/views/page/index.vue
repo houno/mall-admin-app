9fbc4f7ac146e1887e37cca15ac7925bcfd46a77
king <398508730@qq.com>
2021-03-01 17:33:00 +0800

    统计页面实现

--- src/views/page/index.vue
+++ src/views/page/index.vue
@@ -1,5 +1,23 @@
 <template>
     <div>
-        统计
+        <div class="index">
+                <card />
+                <carts />
+        </div>
     </div>
 </template>
+
+<script>
+import Card from '@/components/card.vue';
+
+import Carts from '../../components/carts.vue';
+
+export default {
+  components: {
+    Card,
+    Carts,
+
+  },
+
+};
+</script>
